263220c5d70ec5b002122507faf3dda5422d53d1
mmoftah3 <71080372+mmoftah3@users.noreply.github.com>
2023-11-29 00:00:11 -0500

    Save lives in playerPrefs

--- Assets/Scripts/LivesManager.cs
+++ Assets/Scripts/LivesManager.cs
@@ -1,33 +1,68 @@
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 public class LivesManager : MonoBehaviour
 {
     private int lives = 10;
     public GameObject[] lifeIcons; // life icons 
-    
 
+
+    private string livesKey = "PlayerLives";
+
+    private void Start()
+    {
+
+        LoadPlayerLives();
+    }
+
+    private void SavePlayerLives()
+    {
+      
+        PlayerPrefs.SetInt(livesKey, lives);
+        PlayerPrefs.Save();
+    }
+
+    private void LoadPlayerLives()
+    {
+        
+        if (PlayerPrefs.HasKey(livesKey))
+        {
+            lives = PlayerPrefs.GetInt(livesKey);
+        }
+
+        
+        UpdateLifeIcons();
+    }
+
+    private void UpdateLifeIcons()
+    {
+        
+        for (int i = 0; i < lifeIcons.Length; i++)
+        {
+            lifeIcons[i].SetActive(i < lives);
+        }
+    }
 
     public int GetLives()
     {
         return lives;
     }
+
     public void LoseLife()
     {
-
         if (lives > 0)
         {
-
             lifeIcons[lives - 1].SetActive(false);
             lives--;
 
+            
+            SavePlayerLives(); // save to playerprefs
         }
 
         if (lives == 0)
         {
             Debug.Log("Game Over");
-
         }
     }
 
@@ -39,4 +74,3 @@ public class LivesManager : MonoBehaviour
         }
     }
 }
-
