37700b4957edce8d2471d0db8f26641e1e742ea2
mmoftah3 <71080372+mmoftah3@users.noreply.github.com>
2023-11-28 23:24:51 -0500

    Keep Lives from level to level

--- Assets/Scripts/LivesManager.cs
+++ Assets/Scripts/LivesManager.cs
@@ -6,6 +6,20 @@ public class LivesManager : MonoBehaviour
 {
     private int lives = 10;
     public GameObject[] lifeIcons; // life icons 
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject); // to keep the lives in next level 
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
     public int GetLives()
     {
         return lives;
