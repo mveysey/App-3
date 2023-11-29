02a168ce42ead5b9864e3f186a26163a4634e702
mveysey <mveysey@uwo.ca>
2023-11-28 22:54:23 -0500

    Add level 2 complete

--- Assets/Scripts/EndLevel/Level2Complete.cs
+++ Assets/Scripts/EndLevel/Level2Complete.cs
@@ -1,18 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Level2Complete : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Button exitButton;
+    public Button nextLevelButton;
+    public Button levelSelectorButton;
+
     void Start()
     {
-        
+        Button next = nextLevelButton.GetComponent<Button>();
+        Button select = levelSelectorButton.GetComponent<Button>();
+        Button exit = exitButton.GetComponent<Button>();
+
+        next.onClick.AddListener(LoadNextLevel);
+        select.onClick.AddListener(LoadLevelSelector);
+        exit.onClick.AddListener(ExitGame);
     }
 
-    // Update is called once per frame
-    void Update()
+    void LoadNextLevel()
     {
-        
+        SceneManager.LoadScene("Level3");
+    }
+
+    void LoadLevelSelector()
+    {
+        SceneManager.LoadScene("LevelSelector");
+    }
+
+    void ExitGame()
+    {
+        Application.Quit();
     }
 }
